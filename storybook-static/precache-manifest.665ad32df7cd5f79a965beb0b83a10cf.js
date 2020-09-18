3081f359f99ec8af7dde5ca9998cea36eb9ff88a
Huynh Ba Lai <laihb@aureole.local>
2020-09-18 13:54:47 +0700

    taskbox UI

---
+++ storybook-static/precache-manifest.665ad32df7cd5f79a965beb0b83a10cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54fd1ef03ba641a19395639bfb70c19",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e090b5c0b3644ac7387f.bundle.js"
+  },
+  {
+    "url": "runtime~main.e090b5c0b3644ac7387f.bundle.js"
+  },
+  {
+    "revision": "fe757b9ae101728c1398",
+    "url": "static/css/main.1c7cd5d1.chunk.css"
+  },
+  {
+    "url": "vendors~main.e090b5c0b3644ac7387f.bundle.js"
+  },
+  {
+    "url": "vendors~main.e090b5c0b3644ac7387f.bundle.js.LICENSE.txt"
+  }
+]);
